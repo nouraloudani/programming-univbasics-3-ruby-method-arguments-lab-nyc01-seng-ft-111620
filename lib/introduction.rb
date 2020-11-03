8efef2deb82b0ee1d0c4295942a9cd0c31a70460
Noura Loudani <contact.nouraloudani@gmail.com>
2020-11-03 05:55:38 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,13 @@ def introduction(name)
   "Hi, my name is #{name}."
 end
 
-def introduction_with_language(name, language = )
+def introduction_with_language(name, language)
+  "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language(name, language = "Ruby")
+  "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
